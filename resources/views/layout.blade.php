a27c4c44715fe5bc2d106b33c509f5d35053a898
ptrc01 <111512409+ptrc01@users.noreply.github.com>
2024-12-07 12:46:57 +0700

    Update layout.blade.php

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -16,7 +16,7 @@
 <body>
     <nav class="navbar navbar-expand-lg navbar-light bg-light pt-2 pb-2">
         <div class="container-fluid">
-            <a class="navbar-brand" href="{{ route('client.index') }}">
+            <a class="navbar-brand" href="/home">
                 <img src="{{ asset('storage/logo-navbar.png') }}" width="50px" alt="logo">
             </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -26,13 +26,13 @@
 
                 <ul class="nav justify-content-center">
                     <li class="nav-item">
-                      <a class="nav-link" href="{{ route('client.index') }}">Home</a>
+                      <a class="nav-link" href="/home">Home</a>
                     </li>
                     <li class="nav-item">
-                      <a class="nav-link" href="#">Events</a>
+                      <a class="nav-link" href="/events">Events</a>
                     </li>
                     <li class="nav-item">
-                      <a class="nav-link" href="{{ route('client.aboutUs') }}">About Us</a>
+                      <a class="nav-link" href="/aboutUs">About Us</a>
                     </li>
                 </ul>
             </div>
@@ -54,10 +54,10 @@
                 {{-- @else
 
                     <li>
-                        <a class="btn btn-outline-primary" href="#" role="button">Login</a>
+                        <a class="btn btn-outline-primary" href="/login" role="button">Login</a>
                     </li>
                     <li>
-                        <a class="btn btn-primary" href="#" role="button">Register</a>
+                        <a class="btn btn-primary" href="/register" role="button">Register</a>
                     </li>
 
                 @endauth --}}
