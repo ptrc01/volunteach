c0e60c3add251b80d0f0b85bacc45e7c97e0afa3
ptrc01 <111512409+ptrc01@users.noreply.github.com>
2024-12-10 02:53:47 +0700

    Add files via upload

---
+++ database/seeders/ParticipantSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class ParticipantSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $participants=[
+            // [
+            //     'event_id'=>'1',
+            //     'client_id'=>'3',
+
+            // ],
+            // [
+            //     'event_id'=>'1',
+            //     'client_id'=>'4',
+
+            // ],
+            // [
+            //     'event_id'=>'2',
+            //     'client_id'=>'4',
+
+            // ]
+        ];
+        DB::table('participants')->insert($participants);
+    }
+}
